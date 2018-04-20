35e67a3565bcdf0dadbc7bf38f21696514ad8f3e
danyCorb <danycor2@hotmail.fr>
2018-04-20 16:57:21 +0200

    commentarisation

--- Programmes/Arduino/emitter.c
+++ Programmes/Arduino/emitter.c
@@ -1,54 +1,61 @@
 /********************************************************************/
-// First we include the libraries
+// Inclusion des librairies de gestion des communications avec le thermomètre et la radio
 #include <OneWire.h> 
 #include <DallasTemperature.h>
 #include <RH_ASK.h>
-#include <SPI.h> // Not actualy used but needed to compile
+#include <SPI.h>
 
+// variable de configuration du module radio
 RH_ASK driver;
 
 /********************************************************************/
-// Data wire is plugged into pin 5 on the Arduino 
-#define ONE_WIRE_BUS 5
-#define ARDUINO_ID 1
+// Les connecteurs utilisés 
+#define ONE_WIRE_BUS 5 // Connecteur thermomètre
+#define ARDUINO_ID 1 // connecteur radio
 /********************************************************************/
-// Setup a oneWire instance to communicate with any OneWire devices  
-// (not just Maxim/Dallas temperature ICs) 
+
+// Initialisation du connecteur thermomètre
 OneWire oneWire(ONE_WIRE_BUS); 
 /********************************************************************/
-// Pass our oneWire reference to Dallas Temperature. 
+// Initialisation du capteur
 DallasTemperature sensors(&oneWire);
 /********************************************************************/ 
+
+
+
 void setup(void) 
 { 
- // start serial port 
+ // Initialisation du port série
  Serial.begin(9600); 
+ // initialisation du communicateur
  if (!driver.init())
          Serial.println("init failed");
 
- // Start up the library 
+ // Lancement du capteur
  sensors.begin(); 
 } 
-void loop(void) 
-{ 
- // call sensors.requestTemperatures() to issue a global temperature 
- // request to all devices on the bus 
-/********************************************************************/
- sensors.requestTemperatures(); // Send the command to get temperature readings 
-/********************************************************************/
- float temperature = sensors.getTempCByIndex(0);
- char msg[255]={0};
- sprintf(msg,"%f %d",temperature, ARDUINO_ID);
- driver.send((const uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
 
- Serial.print(temperature); // Why "byIndex"?  
- Serial.print(" ");
- Serial.print(ARDUINO_ID);
- Serial.print("\n");
+
+
+void loop(void) 
+{
+ char msg[255]={0}; // Variable de message d'envoi
  
- Serial.flush();
-   // You can have more than one DS18B20 on the same bus.  
-   // 0 refers to the first IC on the wire 
+ sensors.requestTemperatures(); // Envoi de la commande pour annoncer la lecture
+ float temperature = sensors.getTempCByIndex(0); // Récupération de la température
+ 
+ sprintf(msg,"%f %d",temperature, ARDUINO_ID); // Edition de message à envoyer
+ 
+ driver.send((const uint8_t *)msg, strlen(msg)); // Envoi du message
+ driver.waitPacketSent(); // Attente de confirmation de l'envoi
+
+ Serial.print(temperature); // Écriture de la température
+ Serial.print(" "); // Écriture d'un espace
+ Serial.print(ARDUINO_ID); // Écriture de l'ID de l'Arduino
+ Serial.print("\n"); // Fin du message
+ 
+ Serial.flush(); // Confirmation de l'envoi
+ 
+   // Attente de 30s
    delay(30000); 
 } 
